ec14f57488ab677ce4f3dfec0af69a72ea84b569
Sunny-117 <zhiqiangfu6@gmail.com>
2025-05-22 21:58:13 +0800

    feat: enum and struct example

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,19 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { sum, hello, Animal } from '../index.js'
 
 test('sum from native', (t) => {
   t.is(sum(1, 2), 3)
+})
+
+test('hello from native', (t) => {
+  t.is(hello('world'), 'hello world')
+})
+
+test('Animal from native', (t) => {
+  const animal = new Animal('dog', 1)
+  t.is(animal.name, 'dog')
+  t.is(animal.age, 1)
+  animal.changeName('cat')
+  t.is(animal.name, 'cat')
 })
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,32 @@ extern crate napi_derive;
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
 }
+
+#[napi]
+pub fn hello(name: String) -> String {
+  format!("hello {}", name)
+}
+
+#[napi]
+struct Animal {
+  pub name: String,
+  pub age: i32,
+}
+
+#[napi]
+impl Animal {
+  #[napi(constructor)]
+  pub fn new(name: String, age: i32) -> Self {
+    Animal { name, age }
+  }
+  #[napi]
+  pub fn change_name(&mut self, new_name: String) {
+    self.name = new_name;
+  }
+}
+
+#[napi(string_enum)]
+pub enum Kind {
+  Dog,
+  Cat,
+}
